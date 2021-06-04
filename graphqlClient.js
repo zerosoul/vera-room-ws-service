ab9a9587aef6cb3b501ebe07469015468f85701c
zerosoul <yanggc888@163.com>
2021-06-04 22:58:02 +0800

    refactor: gRequest & utils file

---
+++ graphqlClient.js
@@ -0,0 +1,72 @@
+const { GraphQLClient, gql } = require('graphql-request')
+const gClient = new GraphQLClient('https://g.nicegoodthings.com/v1/graphql')
+const QUERY_ROOM_LIST = gql`
+  query RoomList {
+    portal_room {
+      personal
+      active
+      id
+      name
+      members
+    }
+  }
+`
+const QUERY_ROOM = gql`
+  query Room($id: String!) {
+    portal_room(where: { id: { _eq: $id } }) {
+      personal
+      active
+      id
+      link
+      name
+      members
+    }
+  }
+`
+const QUERY_PERSONAL_ROOM = gql`
+  query Room($creator: String!) {
+    portal_room(
+      where: { creator: { _eq: $creator }, personal: { _eq: true } }
+    ) {
+      id
+      personal
+    }
+  }
+`
+const UPDATE_ACTIVE = gql`
+  mutation UpdateActive($active: Boolean!, $id: String!) {
+    update_portal_room(_set: { active: $active }, where: { id: { _eq: $id } }) {
+      returning {
+        active
+      }
+    }
+  }
+`
+const UPDATE_MEMBERS = gql`
+  mutation UpdateMembers($id: String!, $member: jsonb) {
+    update_portal_room(
+      _prepend: { members: $member }
+      where: { id: { _eq: $id } }
+    ) {
+      returning {
+        members
+      }
+    }
+  }
+`
+const requestHeaders = {
+  'content-type': 'application/json',
+  'x-hasura-admin-secret': 'tristan@privoce',
+}
+const gRequest = (query, payload) => {
+  return gClient.request(query, payload, requestHeaders)
+}
+
+module.exports = {
+  gRequest,
+  QUERY_ROOM_LIST,
+  QUERY_PERSONAL_ROOM,
+  QUERY_ROOM,
+  UPDATE_ACTIVE,
+  UPDATE_MEMBERS,
+}
