329d81bf8b40501c17914dd8136023ec7a0165ac
zerosoul <yanggc888@163.com>
2021-08-29 11:30:35 +0800

    feat: send msg on connected

--- ws.zoom.vera.js
+++ ws.zoom.vera.js
@@ -1,12 +1,12 @@
-// const getRoomInstance = require("./Room");
+const getRoomInstance = require("./Room");
 
 const initZoomVeraSocket = async (io, socket, params = {}) => {
     const { roomId } = params;
     if (!roomId) return;
     socket.join(`${roomId}_zoom`);
     // 立即发送当前数据
-    // const CurrentRoom = await getRoomInstance({ id: roomId });
-    // socket.emit("ZOOM_VERA_DATA", { tabs: CurrentRoom.workspaceData?.tabs || [], users: CurrentRoom.activeUsers });
+    const CurrentRoom = await getRoomInstance({ id: roomId });
+    socket.emit("ZOOM_VERA_DATA", { tabs: CurrentRoom.tabs || [], users: CurrentRoom.activeUsers });
 
     // Leave the room if the user closes the socket
     socket.on("disconnect", (reason) => {
