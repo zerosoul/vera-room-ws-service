b021258362bca4a9a89ae5de5d4f0e928eb57335
zerosoul <yanggc888@163.com>
2021-08-29 12:15:32 +0800

    fix: currentRoom check

--- ws.zoom.vera.js
+++ ws.zoom.vera.js
@@ -1,12 +1,13 @@
-const getRoomInstance = require("./Room");
+const { Rooms } = require("./Room");
 
 const initZoomVeraSocket = async (io, socket, params = {}) => {
     const { roomId } = params;
     if (!roomId) return;
     socket.join(`${roomId}_zoom`);
     // 立即发送当前数据
-    const CurrentRoom = await getRoomInstance({ id: roomId });
-    socket.emit("ZOOM_VERA_DATA", { tabs: CurrentRoom.tabs || [], users: CurrentRoom.activeUsers });
+    const currentRoom = Rooms[roomId] || null;
+    let initData = currentRoom ? { tabs: currentRoom.tabs || [], users: currentRoom.activeUsers } : { tabs: [], users: [] };
+    socket.emit("ZOOM_VERA_DATA", initData);
 
     // Leave the room if the user closes the socket
     socket.on("disconnect", (reason) => {
