9d2ae092c8eb36dc321235cf33f963555cef8f1d
zerosoul <yanggc888@163.com>
2021-11-30 23:05:11 +0800

    fix: authing webhook api

--- index.js
+++ index.js
@@ -88,9 +88,12 @@ server.listen(PORT, () => {
 });
 // APIs
 app.post("/authing/webhook", async (req, res) => {
-  const sig = req.headers["X-Authing-Token"];
+  const sig = req.headers["x-authing-webhook-secret"];
   console.log("authing sig", sig);
-  if (!sig) return;
+  if (!sig) {
+    res.status(401).send();
+    return;
+  }
   const { eventName, data } = req.body;
   switch (eventName) {
     case "login":
